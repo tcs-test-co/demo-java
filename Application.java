b5dd2ff8309c8b72f97972274cbb60e0b78406bc
co-tcs <132944815+co-tcs@users.noreply.github.com>
2023-06-26 10:08:22 -1000

    Update Application.java

--- Application.java
+++ Application.java
@@ -26,9 +26,7 @@ public class Application {
 	public static String connectionString = "jdbc:sqlite:database.sqlite";
 
 	public static final Logger logger = LogManager.getLogger();
-
-	public static String testSecret = "npm_1AAiSUiHSDk1DKIu9IOW0OIWlkPOEWwqTqsJY";
-  
+	
 	public static void main(String[] args) {
 		try {
 			Class.forName("org.sqlite.JDBC");
