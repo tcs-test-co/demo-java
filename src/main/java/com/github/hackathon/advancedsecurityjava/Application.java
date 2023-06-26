427a9d3f87e2dadba0b2e452e3f008a66a4610a4
co-tcs <132944815+co-tcs@users.noreply.github.com>
2023-06-26 09:47:17 -1000

    Update Application.java

--- src/main/java/com/github/hackathon/advancedsecurityjava/Application.java
+++ src/main/java/com/github/hackathon/advancedsecurityjava/Application.java
@@ -16,8 +16,6 @@ import com.github.hackathon.advancedsecurityjava.Models.Book;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-string testSecret = "scuah_asdeurj12n34n128dchd83h5";
-
 @SpringBootApplication
 public class Application {
 
